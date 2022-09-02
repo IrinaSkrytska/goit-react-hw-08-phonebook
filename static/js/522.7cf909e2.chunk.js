468426dac3fcb954a81dbd14cb769f556e66f387
IrinaSkrytska <93982819+IrinaSkrytska@users.noreply.github.com>
2022-09-02 20:57:51 +0000

    Deploying to gh-pages from @ IrinaSkrytska/goit-react-hw-08-phonebooki@86b040807e10667c20f41d6ebdb7804d20dfa608 🚀

---
+++ static/js/522.7cf909e2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[522],{8522:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(184);function c(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)("section",{children:(0,r.jsx)("h1",{children:"Home Page"})})})}}}]);
+//# sourceMappingURL=522.7cf909e2.chunk.js.map
